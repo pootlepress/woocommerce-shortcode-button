5554d2c2595f88ac96d3e9c24e943bf3e4a67258
Alan Ng <jenfoong@gmail.com>
2014-07-18 17:15:22 +0800

    Add Updater

--- pootlepress-wx-shortcode-button.php
+++ pootlepress-wx-shortcode-button.php
@@ -14,8 +14,21 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 require_once( 'pootlepress-wx-shortcode-button-functions.php' );
 require_once( 'classes/class-pootlepress-wx-shortcode-button.php' );
+require_once( 'classes/class-pootlepress-updater.php');
 
 $GLOBALS['pootlepress_wx_shortcode_button'] = new Pootlepress_Wx_Shortcode_Button( __FILE__ );
 $GLOBALS['pootlepress_wx_shortcode_button']->version = '1.0.0';
 
+add_action('init', 'pp_wsb_updater');
+function pp_wsb_updater()
+{
+    if (!function_exists('get_plugin_data')) {
+        include(ABSPATH . 'wp-admin/includes/plugin.php');
+    }
+    $data = get_plugin_data(__FILE__);
+    $wptuts_plugin_current_version = $data['Version'];
+    $wptuts_plugin_remote_path = 'http://jamiemarsland.staging.wpengine.com/?updater=1';
+    $wptuts_plugin_slug = plugin_basename(__FILE__);
+    new Pootlepress_Updater ($wptuts_plugin_current_version, $wptuts_plugin_remote_path, $wptuts_plugin_slug);
+}
 ?>
